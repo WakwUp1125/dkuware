de050df1189d1231f2ccc21c22b24a8091f889a2
choijieun <ppwando091@naver.com>
2023-11-29 10:54:49 +0900

    first commit

---
+++ threadpool.h
@@ -0,0 +1,17 @@
+//header guard 
+#ifndef THREADPOOL
+#define THREADPOOL
+
+
+
+#define QUEUE_SUCCESS   0
+#define QUEUE_REJECTED  1
+
+
+void execute(void (*func)(void *param), void *param);
+int pool_submit(void (* func)(void *param), void *param);
+void *worker(void *param);
+void pool_init(void);
+void pool_shutdown(void);
+
+#endif
